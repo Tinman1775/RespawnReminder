1630c752b36d0a4551b1ad89e494cd2a90919e6e
Director Krennic <tinman44281@gmail.com>
2021-05-07 14:10:25 -0400

    Initial Upload

---
+++ ETPluginMeme/Base.cs
@@ -0,0 +1,17 @@
+﻿namespace RespawnReminder
+{
+    using HarmonyLib;
+    using VirtualBrightPlayz.SCP_ET.Player;
+
+    [HarmonyPatch(typeof(PlayerStats), nameof(PlayerStats.ClientChangeClass))]
+    public class Base
+    {
+        public static void Prefix(PlayerStats __instance, int old, int id)
+        {
+            if (id == 0)
+            {
+                __instance.ctrl.textChatScript.RpcAddMessage("Remember to use /respawn to respawn!", "blue");
+            }
+        }
+    }
+}
---
+++ ETPluginMeme/FunnyMonkeyFramework.cs
@@ -0,0 +1,19 @@
+﻿namespace ETPluginMeme
+{
+    using HarmonyLib;
+
+    class FunnyMonkeyFramework : PluginFramework.Plugin
+    {
+        public Harmony Harmony;
+
+        public override void OnDisable()
+        {
+        }
+
+        public override void OnEnable()
+        {
+            Harmony = new Harmony("Monkey");
+            Harmony.PatchAll();
+        }
+    }
+}
